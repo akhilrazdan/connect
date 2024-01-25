04fcd9d86aa7339d8f405985aeaf016b4f18df88
Akhil Razdan <akhilz@amazon.com>
2024-01-25 14:28:02 -0800

    Some css changes for adding padding

--- src/components/grouped-mentors/grouped-mentors.component.jsx
+++ src/components/grouped-mentors/grouped-mentors.component.jsx
@@ -2,19 +2,20 @@ import { useContext, Fragment } from 'react';
 
 import { MentorsContext } from '../../contexts/mentors.context';
 import MentorList from '../MentorList/mentor-list.component';
+import './grouped-mentors.styles.scss'
 
 const GroupedMentors = () => {
   const { mentorsGroupedByIaf: mentorsMap } = useContext(MentorsContext);
   console.log("In Grouped mentors")
   return (
-    <Fragment>
+    <div className='grouped-mentors'>
       {Object.keys(mentorsMap).map((iaf_name) => {
         const mentors = mentorsMap[iaf_name];
         return (
           <MentorList key={iaf_name} iaf={iaf_name} mentors={mentors} />
         );
       })}
-    </Fragment>
+    </div>
   );
 };
 
