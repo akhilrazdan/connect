e391d21a4428ed3e7070a6e224a70e9af1e7ccf4
Akhil Razdan <akhilz@amazon.com>
2024-01-26 09:38:30 -0800

    Make the sign in a modal

--- src/components/authentication/authentication.component.jsx
+++ src/components/authentication/authentication.component.jsx
@@ -3,21 +3,33 @@ import SignUpForm from "../sign-up-form/sign-up-form.component";
 import SignInForm from "../sign-in-form/sign-in-form.component";
 import './authentication.styles.scss';
 import { useNavigate } from "react-router-dom";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UnifiedUserContext } from "../../contexts/unified-user.context";
 
 const Authentication = () => {
-    const navigate = useNavigate()
-    const { isSignInProcessComplete, currentUser } = useContext(UnifiedUserContext)
+    const navigate = useNavigate();
+    const { isSignInProcessComplete, currentUser } = useContext(UnifiedUserContext);
+    const [formToShow, setFormToShow] = useState('signIn'); // 'signIn' or 'signUp'
+
     useEffect(() => {
         if (currentUser) {
-            navigate('/')
+            navigate('/');
         }
-    }, [currentUser, isSignInProcessComplete]);
+    }, [currentUser, isSignInProcessComplete, navigate]);
 
-    return (<div className="authentication-container">
-        <SignInForm />
-        <SignUpForm />
-    </div>)
-}
+    return (
+        <div className="authentication-container">
+            {formToShow === 'signIn' && <SignInForm />}
+            {formToShow === 'signUp' && <SignUpForm />}
+            <div className="switch-form">
+                {formToShow === 'signIn' ? (
+                    <p>New here? <span onClick={() => setFormToShow('signUp')}>Sign Up</span></p>
+                ) : (
+                    <p>Already have an account? <span onClick={() => setFormToShow('signIn')}>Sign In</span></p>
+                )}
+            </div>
+        </div>
+    );
+};
+
 export default Authentication;
--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -72,7 +72,7 @@ const SignUpForm = () => {
     return (
         <div className="sign-up-container">
             <h2>Don't have an account?</h2>
-            <span>Sign up with your email and password</span>
+            <span>Sign up with your email and password. </span>
             <form onSubmit={handleSubmit}>
                 <FormInput label="Display name" required type="text" onChange={handleChange} name="displayName" value={displayName} />
 
