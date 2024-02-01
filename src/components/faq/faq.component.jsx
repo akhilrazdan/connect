474682d0c689ec597d95e4dff6ef664a3800831a
Akhil Razdan <akhilz@amazon.com>
2024-02-01 01:56:55 -0500

    Add FAQ

--- src/components/faq/faq.component.jsx
+++ src/components/faq/faq.component.jsx
@@ -2,15 +2,102 @@ import './faq.styles.scss'
 const Faq = () => {
     console.log('Inside FAQ')
     return (
-        <div className="faq-container">
-            <h2>How this works?</h2>
-            <p>A mentee gets to select 3 choices</p>
-            <p>A mentor has to slots gets to select 3 choices</p>
+        <div className="mentorship-faq">
+            <h1>Mentorship FAQ</h1>
+            <div className="faq-content">
+                <div className="question">How does this work?</div>
+                <p>
+                    You can view the Mentors under the Industry Areas of Focus for your “track”
+                    As a reminder, the three tracks are: </p>
+                <p>
+                    <p>
+                        <b>Production Track:</b> Cellar/Production and Viticulture <br />
+                        <b>Sales Track:</b> Retail, Sales & Distribution, Hospitality<br />
+                        <b>Brand Management Track:</b> Marketing & Communications, Wine Business<br />
+                    </p>
+                    <p>
+                        You can select up to 3 mentors (minimum: 1, maximum 3) from your “track list”. Mentor selection is <b>FIRST COME, FIRST SERVE</b>. <br />
+                        Once you click on a mentor, your selection is FINAL.<br />
+                        In case of errors, please contact <span className='email'>mentorship@batonnageforum.com</span> <br />
+                    </p>
+                </p>
 
-            <h2>When will you get in touch with me?</h2>
-            <p>Dec 2024</p>
-        </div>
-    )
+                <div className='question'>Why can’t I click on a Mentor?</div>
+                <p>
+                    If your Mentor is tagged "List Full", this means that their mentee intake list is full. If you are interested in speaking with a mentor, contact mentorship@batonnageforum.com to get added to a waitlist. We cannot guarantee that you will be connected with the mentor as this is decided on a case by case basis and mentor availability.
+                    <br />
+                    In case of any other errors, try refreshing your browser. We recommend that you use Google Chrome for optimal functionality. It is preferred that you use a desktop for better reads instead of a phone/tablet.
+                </p>
+
+                <div className="question">How do I know which mentor I should pick?</div>
+                <p>
+                    Click on the Mentor name to read their full bio. This will help you decide if they are a good match for you based on your current interests, skills, geographical location, etc.
+                    For additional support, contact your IAF Head or Mentee Liaison Officer. Contact information is below:
+                </p>
+                <p>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>IAF Heads</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Preferred E-mail</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Retail</td>
+                                <td>Annette</td>
+                                <td>Alvarez Peters</td>
+                                <td>annetteapeters@gmail.com</td>
+                            </tr>
+                            <tr>
+                                <td>Cellar/Production</td>
+                                <td>Jessica</td>
+                                <td>Koga</td>
+                                <td>jkoga@schramsberg.com</td>
+                            </tr>
+                            <tr>
+                                <td>Wine Business</td>
+                                <td>Emily</td>
+                                <td>Porter</td>
+                                <td>emilyporter.winebusiness@gmail.com</td>
+                            </tr>
+                            <tr>
+                                <td>Mentorship Director</td>
+                                <td>Priyanka</td>
+                                <td>French</td>
+                                <td>mentorship@batonnageforum.com</td>
+                            </tr>
+                            <tr>
+                                <td>Executive Director</td>
+                                <td>Michelle</td>
+                                <td>Schromm</td>
+                                <td>info@batonnageforum.com</td>
+                            </tr>
+                            <tr>
+                                <td>Mentee Liaison Officer</td>
+                                <td>Terah</td>
+                                <td>Bajjalieh</td>
+                                <td>terahcb@gmail.com</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </p>
+                <div className='question'>How do I know which Mentors I am able to interact with?</div>
+                <p>You will receive email confirmations with your mentor final lists. Additionally you can log into the account on this platform to view your final mentor lists as well. This will be available on Feb 11th, 2024.</p>
+                <div className='question'>I forgot my login information, what do I do?</div>
+                <p>Unfortunately, we don’t have the "Forgot Password" option available yet. Please contact <span className='email'>mentorship@batonnageforum.com</span> for next steps for your account information.</p>
+
+
+                <div className='question'>I haven’t heard from my Mentor, what do I do?</div>
+                <p>
+                    If you haven’t received any contact or communication from any Mentors by April 10th, 2024, please contact mentorship@batonnageforum.com. Additionally your IAF heads are also available to help connect you with your mentors as well.
+                </p>
+
+            </div>
+        </div >
+    );
 }
 
 export default Faq;
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,4 +1,4 @@
-import { Outlet, NavLink } from "react-router-dom";
+import { Outlet, NavLink, Link } from "react-router-dom";
 import { Fragment, useContext } from "react";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import CrwnLogo from '../../assets/BatonnageLogo_Orange_Mentorship.png';
@@ -16,11 +16,12 @@ const Navigation = () => {
     return (
         <Fragment>
             <div className="navigation">
-                <NavLink className="logo-container" to='/'>
-                    <img src={CrwnLogo} alt="Logo" className="logo-image" />
-                </NavLink>
+
                 <div className="nav-links-container">
                     <div className="left-nav-links">
+                        <Link className="logo-container" to='/'>
+                            <img src={CrwnLogo} alt="Logo" className="logo-image" />
+                        </Link>
                         <NavLink to='/' className="nav-link home" activeClassName="active-link">
                             Home
                         </NavLink>
